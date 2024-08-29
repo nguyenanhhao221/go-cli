6bfc15b1e8a7e9f0f919cf8344428a5d67087e48
Hao Nguyen <hao@haonguyen.tech>
2024-08-30 02:51:20 +0700

    test: add TestComplete

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -14,3 +14,22 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected %q, got %q instead", taskName, l[0].Task)
 	}
 }
+
+func TestComplete(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "New Task"
+	l.Add(taskName)
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead", taskName, l[0].Task)
+	}
+
+	if l[0].Done {
+		t.Errorf("New Task should not be completed")
+	}
+
+	_ = l.Complete(1)
+	if !l[0].Done {
+		t.Errorf("This task should be completed")
+	}
+}
