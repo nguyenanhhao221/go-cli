7b6811409fb257f8ab2ec16d29fb1927f4ab05c2
Hao Nguyen <hao@haonguyen.tech>
2024-08-30 16:36:31 +0700

    feat: add Save and Get for json handling

--- todo/todo.go
+++ todo/todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -50,4 +53,25 @@ func (l *List) Delete(i int) error {
 	*l = append(ls[:i-1], ls[i:]...)
 	return nil
 
+}
+
+func (l *List) Save(filename string) error {
+	js, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, js, 0644)
+
+}
+
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	return json.Unmarshal(file, l)
 }
