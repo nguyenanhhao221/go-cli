10f34a4f159f667da21b3116de21c8086c280a61
Hao Nguyen <hao@haonguyen.tech>
2024-09-02 01:50:53 +0700

    feat: add custom usage information

--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -10,6 +10,13 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s tool. Developed By Hao Nguyen\n", os.Args[0])
+		fmt.Fprintln(flag.CommandLine.Output(), "Copyright 2024")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage Information:")
+		flag.PrintDefaults()
+	}
+	// Define some flags options
 	task := flag.String("task", "", "Task to be included in the ToDo List")
 	list := flag.Bool("list", false, "List the tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
