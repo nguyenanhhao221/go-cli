595aca21894ca576fd5688c16900f70aa0048aba
Hao Nguyen <hao@haonguyen.tech>
2024-09-02 18:45:48 +0700

    chore: test list task use string convert instead

--- todo/cmd/todo/main_test.go
+++ todo/cmd/todo/main_test.go
@@ -82,7 +82,7 @@ func TestTodoCLI(t *testing.T) {
 		expected := fmt.Sprintf(" 1: %s\n 2: %s\n", task, task2)
 
 		if expected != string(out) {
-			t.Errorf("Expected %q, got %q instead \n", expected, string(out))
+			t.Errorf("Expected %q, got %s instead \n", expected, out)
 		}
 	})
 }
