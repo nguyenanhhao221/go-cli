da250f26c1985bfe57336a69cd8b041bcbeebcad
Hao Nguyen <hao@haonguyen.tech>
2024-09-06 21:31:44 +0700

    test: add test for run func

--- markdown_preview/main_test.go
+++ markdown_preview/main_test.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	inputFile  = "./testdata/test1.md"
-	resultFile = "test1.html"
+	resultFile = "test1.md.html"
 	goldenfile = "./testdata/test1.html"
 )
 
@@ -33,4 +33,27 @@ func TestParseContent(t *testing.T) {
 		t.Errorf("Result parseContent does not match golden file:\n%s", cmp.Diff(expected, result))
 	}
 
+}
+
+func TestRun(t *testing.T) {
+	if err := run(inputFile); err != nil {
+		t.Fatal(err)
+	}
+	result, err := os.ReadFile(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := os.ReadFile(goldenfile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(result, expected) {
+		t.Logf("golden:\n%s\n", expected)
+		t.Logf("result:\n%s\n", result)
+		t.Errorf("Result content does not match golden file:\n%s", cmp.Diff(expected, result))
+	}
+
+	os.Remove(resultFile)
 }
