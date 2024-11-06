e6bd6d2c3c7cc4752d9a3bff1a9dbd1aec1875d5
Hao Nguyen <hao@haonguyen.tech>
2024-11-06 22:10:54 +0700

    feat: add cobra config

--- interactiveTools/pomo/cmd/repoinmemory.go
+++ interactiveTools/pomo/cmd/repoinmemory.go
@@ -19,10 +19,13 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package main
+package cmd
 
-import "haonguyen.tech/interactiveTools/pomo/cmd"
+import (
+	"haonguyen.tech/interactiveTools/pomo/pomodoro"
+	"haonguyen.tech/interactiveTools/pomo/pomodoro/repository"
+)
 
-func main() {
-	cmd.Execute()
+func getRepo() (pomodoro.Repository, error) {
+	return repository.NewInMemoryRepo(), nil
 }
---
+++ interactiveTools/pomo/cmd/root.go
@@ -0,0 +1,121 @@
+/*
+Copyright © 2024 Hao Nguyen <hao@haonguyen.tech>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"haonguyen.tech/interactiveTools/pomo/app"
+	"haonguyen.tech/interactiveTools/pomo/pomodoro"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "pomo",
+	Short: "A brief description of your application",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		repo, err := getRepo()
+		if err != nil {
+			return err
+		}
+		config := pomodoro.NewConfig(
+			repo,
+			viper.GetDuration("pomo"),
+			viper.GetDuration("short"),
+			viper.GetDuration("long"),
+		)
+
+		return rootAction(os.Stdout, config)
+	},
+}
+
+func rootAction(w io.Writer, config *pomodoro.IntervalConfig) error {
+	a, err := app.New(config)
+	if err != nil {
+		return err
+	}
+	return a.Run()
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%q", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pomo.yaml)")
+
+	rootCmd.Flags().DurationP("pomo", "p", 25*time.Minute, "Pomodoro duration")
+	rootCmd.Flags().DurationP("short", "s", 5*time.Minute, "Short break duration")
+	rootCmd.Flags().DurationP("long", "l", 15*time.Minute, "Long break duration")
+
+	err := viper.BindPFlag("pomo", rootCmd.Flags().Lookup("pomo"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "BindPFlag %s", err)
+	}
+	err = viper.BindPFlag("short", rootCmd.Flags().Lookup("short"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "BindPFlag %s", err)
+	}
+	err = viper.BindPFlag("long", rootCmd.Flags().Lookup("long"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "BindPFlag %s", err)
+	}
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".pomo" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".pomo")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
